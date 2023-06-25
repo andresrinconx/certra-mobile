9d04de26868f8891e6374bd10afa3d74a6c507d7
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-06-25 13:12:03 -0400

    carrito finalizado

---
+++ src/components/views/caritooooooo.js
@@ -0,0 +1,29 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const caritooooooo = () => {
+    const name = () => {
+      
+    }
+
+
+    const fe = () => {
+      
+    }
+
+    function holaholahola() {
+      
+    }
+    holaholahola()
+
+
+    
+  return (
+    <View>
+      <Text>carito
+        oooooo</Text>
+    </View>
+  )
+}
+
+export default caritooooooo
