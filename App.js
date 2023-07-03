0bbe5e4beb70031ad7fee9edf508e5b96dcd9fb8
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-07-03 09:15:56 -0400

    cambios

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
 import Navigation from './src/navigation/Navigation';
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-import Inicio from './src/components/views/Inicio';
-import Cart from './src/components/views/Cart';
-import Login from './src/components/views/Login';
-
-import Carrito from './src/components/ui/Carrito';
-
-const Stack = createNativeStackNavigator()
-
 const App = () => {
   return(
     <Navigation />
