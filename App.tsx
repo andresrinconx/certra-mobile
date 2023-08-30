07b660c80f516ff6c7647147472da1d7aeb0abd8
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-08-30 15:15:52 -0400

    instaling native-base

--- App.tsx
+++ App.tsx
@@ -1,15 +1,25 @@
-import Navigation from './src/components/Navigation'
-import { InvProvider } from './src/context/InvProvider'
-import { LoginProvider } from './src/context/LoginProvider'
+// import Navigation from './src/components/Navigation'
+// import { InvProvider } from './src/context/InvProvider'
+// import { LoginProvider } from './src/context/LoginProvider'
 
-const App = () => {
-  return ( 
-    <LoginProvider>
-      <InvProvider>
-        <Navigation />
-      </InvProvider>
-    </LoginProvider>
-  )
+// const App = () => {
+//   return ( 
+//     <LoginProvider>
+//       <InvProvider>
+//         <Navigation />
+//       </InvProvider>
+//     </LoginProvider>
+//   )
+// }
+
+// export default App
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Box>Hello world</Box>
+    </NativeBaseProvider>
+  );
 }
-
-export default App
