2f2b3a7b9646bb9b4fa9597049a6162f6ccbecfb
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-08-31 12:30:19 -0400

    logo screen

--- src/components/loaders/LoaderLogoScreen.tsx
+++ src/components/loaders/LoaderLogoScreen.tsx
@@ -1,14 +1,45 @@
-import { View, Image } from 'react-native'
-import { theme } from '../../styles'
+import React, { useState, useEffect } from 'react';
+import { View, Image, Animated, Easing } from 'react-native';
+import useLogin from '../../hooks/useLogin';
 
 const LoaderLogoScreen = () => {
-  return (
-    <View className="flex-1 bg-blue-400">
-      <View className='flex-1 justify-center items-center'>
-        {/* <Image source={require('../../assets/user.png')} style={{width: 200, height: 200,}} /> */}
-      </View>
-    </View>
-  )
-}
+  const { themeColors: { primary } } = useLogin();
+  const [rotationValue] = useState(new Animated.Value(0));
 
-export default LoaderLogoScreen
+  useEffect(() => {
+    const rotateImage = () => {
+      Animated.timing(rotationValue, {
+        toValue: 1,
+        duration: 1500, // Ajusta la duración según tus necesidades
+        easing: Easing.linear,
+        useNativeDriver: false,
+      }).start(() => {
+        rotationValue.setValue(0);
+        rotateImage();
+      });
+    };
+
+    rotateImage();
+  }, []);
+
+  const rotateInterpolation = rotationValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  return (
+    <View className='flex-1 justify-center items-center' style={{ backgroundColor: primary || '#fff' }}>
+      <Animated.Image
+        source={require('../../assets/pastilla.png')}
+        style={{
+          transform: [{ rotate: rotateInterpolation }],
+          width: 100,
+          height: 100,
+        }}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
+
+export default LoaderLogoScreen;
