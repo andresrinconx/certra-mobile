90fca80592c99c27905ebbfe9934675bebdb8701
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2023-09-25 14:37:29 -0400

    bugs fixed

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -24,7 +24,7 @@ const Products = () => {
         getProducts()
       }
     }
-  }, [currentPage])
+  }, [currentPage, flowControl?.showProducts])
 
   // hide keyboard
   const handleScroll = () => {
