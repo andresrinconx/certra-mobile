999c08b1a7a635361134687cdbf379cd67c330d8
andresrinconx <106491925+andresrinconx@users.noreply.github.com>
2024-01-11 08:19:08 -0400

    adds

--- src/components/customer/LabelCustomer.tsx
+++ src/components/customer/LabelCustomer.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { View, Text } from 'react-native';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 const LabelCustomer = ({name}: {name: string}) => {
+  const [psicotropicos, setPsicotropicos] = useState(false);
+
   return (
     <View>
       <Text className='font-extrabold text-typography' style={{ fontSize: wp(4.5) }}>Cliente</Text>
